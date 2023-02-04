19c1bac950569af10ed7e7cd2d5d4c6f475dc3ca
luciaguerra <luciaguerra441@gmail.com>
2023-02-04 19:18:56 -0300

    v1.0.2

--- js/codigo.js
+++ js/codigo.js
@@ -1,28 +1,45 @@
-let num1 = 8;
-let num2 = 2;
+let puntajeUno = 0;
+let puntajeDos = 0;
 
-document.getElementById("num1-el").textContent = num1;
-document.getElementById("num2-el").textContent = num2;
+let resultadoUno = document.getElementById("puntaje-1");
+let resultadoDos = document.getElementById("puntaje-2");
 
-let sumEl = document.getElementById("sum-el");
-
-
-function add() {
-    let suma = num1 + num2;
-    sumEl.textContent = "Sum: " + suma;
+function add1Point_1() {
+    puntajeUno += 1;
+    resultadoUno.textContent = puntajeUno;
 }
 
-function substract() {
-    let resta = num1 - num2;
-    sumEl.textContent = "Sum: " + resta;
+function add5Points_1() {
+    puntajeUno += 5;
+    resultadoUno.textContent = puntajeUno;
 }
 
-function divide(){
-    let division = num1 / num2;
-    sumEl.textContent = "Sum: " + division;
+function add10Points_1() {
+    puntajeUno += 10;
+    resultadoUno.textContent = puntajeUno;
 }
 
-function multiply(){
-    let multiplicacion = num1 * num2;
-    sumEl.textContent = "Sum: " + multiplicacion;
+function subtract10Points_1() {
+    puntajeUno -= 10;
+    resultadoUno.textContent = puntajeUno;
+}
+
+function add1Point_2() {
+    puntajeDos += 1;
+    resultadoDos.textContent = puntajeDos;
+}
+
+function add5Points_2() {
+    puntajeDos += 5;
+    resultadoDos.textContent = puntajeDos;
+}
+
+function add10Points_2() {
+    puntajeDos += 10;
+    resultadoDos.textContent = puntajeDos;
+}
+
+function subtract10Points_2() {
+    puntajeDos -= 10;
+    resultadoDos.textContent = puntajeDos;
 }
